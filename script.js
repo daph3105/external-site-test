14d7a29e5f1388289e1d00aac257ed436bcdf704
Daphne Feresin <dferesin@hubspot.com>
2022-03-08 17:35:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("button1").style.color = "red";
+  }
+
+  function myFunction2() {
+    document.getElementById("button2").style.color = "blue";
+  }
