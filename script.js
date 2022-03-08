4b9d072b137a9debd723d462753a49f90538e602
Daphne Feresin <dferesin@hubspot.com>
2022-03-08 17:56:37 -0500

    HubSpot tracking event

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function myFunction() {
-    document.getElementById("button1").style.color = "red";
-  }
+// function myFunction() {
+//     document.getElementById("button1").style.color = "red";
+//   }
 
   function myFunction2() {
     document.getElementById("button2").style.color = "blue";
   }
+
+  /*
+Example code to fire a custom behavioral event
+*/
+
+document.getElementById("button1").onclick = function() {_hsq.push(["trackCustomBehavioralEvent", {
+    name: "pe20777930_page_1",
+    properties: {
+        hs_page_path: "button 1"
+    }
+ }])
+ document.getElementById("button1").style.color = "red";
+};
