8fb2b1e0d8f60db9abeb0683f1aa1aaf65aa107c
Daphne Feresin <dferesin@hubspot.com>
2022-03-14 18:13:59 -0400

    removed local js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //     document.getElementById("button1").style.color = "red";
 //   }
 
-  function myFunction2() {
-    document.getElementById("button2").style.color = "blue";
-  }
+  // function myFunction2() {
+  //   document.getElementById("button2").style.color = "blue";
+  // }
 
   
 
