252f04f57dd1e942165d098e69698c576cd15889
Daphne Feresin <dferesin@hubspot.com>
2022-03-14 15:33:54 -0400

    added visitor email

--- script.js
+++ script.js
@@ -6,36 +6,7 @@
     document.getElementById("button2").style.color = "blue";
   }
 
-  var app = new Vue({
-  el: "#app-cbe",
-  data: {
-    email: "",
-    software_version: "(this value will update after selecting a Software Version)",
-    dashboard_name: "(this value will update after selecting a Dashboard Name)",
-  },
-  delimiters: ["[[", "]]"],
-  methods: {
-    fireCustomBehavioralEvent: function () {
-      //Add your CBE JS here
-      _hsq.push([
-        "identify",
-        {
-          email: this.email,
-        },
-      ]);
-      _hsq.push([
-        "trackCustomBehavioralEvent",
-        {
-          name: "pe20777930_software_usage_event",
-          properties: {
-            software_version: this.software_version,
-            dashboard_name: this.dashboard_name,
-          },
-        },
-      ]);
-    },
-  }
-});
+  
 
   /*
 Example code to fire a custom behavioral event
